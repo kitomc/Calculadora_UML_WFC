58d0654d59f3021f364d3287bbdc5265c962ddbd
Francis Gonzalez <kitomc.rd@gmail.com>
2018-12-19 01:00:03 -0400

    nueva clase claculadora agragada a UML

---
+++ Calculadora_UML_WPF/Capa_Negocio/Calculadora.cs
@@ -0,0 +1,34 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Calculadora {
+
+    public Calculadora() {
+    }
+
+    public void double resultado;
+
+    public void double Cal_1;
+
+    public void double Cal_2;
+
+    public void suma() {
+        // TODO implement here
+    }
+
+    public void resta() {
+        // TODO implement here
+    }
+
+    public void division() {
+        // TODO implement here
+    }
+
+    public void multiplicacion() {
+        // TODO implement here
+    }
+
+}
